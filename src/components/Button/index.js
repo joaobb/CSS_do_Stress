8e61ba1fe11a1644d022b86c4ef6e38938560b9b
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2021-10-22 10:33:42 -0300

    vamo

---
+++ src/components/Button/index.js
@@ -0,0 +1,17 @@
+import styles from './Button.module.scss';
+
+function Button() {
+    return (
+        <div className={styles.button}>
+            
+            <p className={styles.title}>
+                CRIAR
+            </p>
+
+            
+        </div>
+        
+    );
+}
+
+export default Button;
